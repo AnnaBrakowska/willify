99df458905d4d85129318ec49ccfd4e787ac23a6
Anna <anna.brakowska91@gmail.com>
2018-10-03 18:22:14 -0400

    sfkjbndkjb

---
+++ script.js
@@ -0,0 +1,10 @@
+let mainImage = document.getElementsByClassName("g-overlay");
+mainImage[0].src =
+  "https://media.bizj.us/view/img/10197713/codesmithwillsentance*750xx2048-1154-0-0.jpg";
+let underneath = document.getElementsByClassName("g-underneath");
+underneath[0].src =
+  "https://static.frontendmasters.com/assets/videos/promo/h43mdlt9aj/720.jpg";
+let title = document.getElementsByClassName("bank");
+title.innerText = "Special Investigation On Execution Context";
+let title2 = document.getElementsByClassName("hed");
+title2[0].innerText = "New Discovery in Functional Programming - The Backpack";
